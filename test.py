b844670deae76c4536799793261bb1754f60e95e
tom56561 <tom56561@gmail.com>
2024-12-10 14:26:28 +0800

    add ocr engine test

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
-import matplotlib
-import numpy
-import pandas
+import pytesseract
 import cv2
 
-img = cv2.imread('images/elephant.png', 0)
 
-# Display the image
-cv2.imshow('image', img)
+img = cv2.imread('images/earth-C.JPG', 0)
+# cv2.imshow('Original Image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# Apply OCR
+text = pytesseract.image_to_string(img)
+print("Extracted Text:")
+print(text)
+lines = text.split('\n')
+
+for line in lines:
+    line = line.strip()
+    if line:
+        print(line)
